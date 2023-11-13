86f9ebafafaae2a9d6d58d675a6c8984d08c3dbf
dantheman <danogbo0@gmail.com>
2023-11-13 20:06:31 +0100

    recent commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const helloWorld = () => {
+        console.log('Hello, world!')
+    }
+    
+    const helloWorldAgain = (name) => {
+        console.log('Hello, ' + name + '!')
+    }
+
     return ( 
         <div className="home">
             <h1>This is the Home Page</h1>
+            <button onClick={helloWorld}>click me</button>
+            <button onClick={ () => helloWorldAgain('mario') }>click me again</button>
         </div>
      );
 }
