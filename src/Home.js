9cab29aea8c5cb0a2320c34159156389fd456972
dantheman <danogbo0@gmail.com>
2023-11-14 05:52:16 +0100

    useState hooks

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,24 @@
+import { useState } from "react";
+
 const Home = () => {
 
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(77);
     const helloWorld = () => {
-        console.log('Hello, world!')
+        setName('luigi');
+        setAge(34)
     }
     
-    const helloWorldAgain = (name) => {
-        console.log('Hello, ' + name + '!')
-    }
+    // const helloWorldAgain = (name) => {
+    //     console.log('Hello, ' + name + '!')
+    // }
 
     return ( 
         <div className="home">
             <h1>This is the Home Page</h1>
+            <p> { name } is { age } years old </p>
             <button onClick={helloWorld}>click me</button>
-            <button onClick={ () => helloWorldAgain('mario') }>click me again</button>
+            {/* <button onClick={ () => helloWorldAgain('mario') }>click me again</button> */}
         </div>
      );
 }
