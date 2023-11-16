81c09e424e71d556d4e1ee09a25cce37b7e002aa
dantheman <danogbo0@gmail.com>
2023-11-16 19:58:10 +0100

    useState, list outputting and mapping

--- src/Home.js
+++ src/Home.js
@@ -2,23 +2,24 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(77);
-    const helloWorld = () => {
-        setName('luigi');
-        setAge(34)
-    }
-    
-    // const helloWorldAgain = (name) => {
-    //     console.log('Hello, ' + name + '!')
-    // }
+    const [blogs, setBlogs] = useState([
+        // an arrya of blogs
+        { title: 'Eat and sleep', body: 'The man who ate before sleeping...', author: 'Dan', id: 1 },
+        { title: 'Love is inevitable', body: 'Widow and homeless...', author: 'Michael Morales', id: 2 },
+        { title: 'Cake and Lovers', body: 'The white and barren...', author: 'Anthony Columbus', id: 3 }
+    ]);
 
     return (  
         <div className="home">
-            <h1>This is the Home Page</h1>
-            <p> { name } is { age } years old </p>
-            <button onClick={helloWorld}>click me</button>
-            {/* <button onClick={ () => helloWorldAgain('mario') }>click me again</button> */}
+            {/* for blog in blogs. so we'll use 'blog' to loop through */}
+            { blogs.map( (blog) => (
+                // never forget to add the key property
+                // the key property helps react to keep track of each item renedered to the DOM
+                <div className="blog-preview" key={blog.id}>
+                    <h1> Title: { blog.title } </h1>
+                    <p> Written by: { blog.author } </p>
+                </div>
+            ) ) }
         </div>
      );
 }
