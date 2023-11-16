d4ead0765896a8596525f3af6f4a8e1b562a3704
dantheman <danogbo0@gmail.com>
2023-11-16 20:24:01 +0100

    props method 1

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 # useState Hooks
 
 This hooks are used when making reactive elements. Elements that do something on listening to an event.
+
+# Props
+
+Helps for reusable components.
+It's basically a way to pass data from a data component to a child component
---
+++ src/BlogList.js
@@ -0,0 +1,15 @@
+const BlogList = (props) => {
+    const blogs = props.blogs
+    return ( 
+        <div className="blog-list">
+            { blogs.map( (blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h1> Title: { blog.title } </h1>
+                    <p> Written by: { blog.author } </p>
+                </div>
+            ) ) }
+        </div>
+     );
+}
+ 
+export default BlogList;
